be0d79ac3bf0b2da10b3e21f5b2844422f86106a
tranminhwang <j2team.tranminhquang@gmail.com>
2023-02-09 12:07:46 +0700

    rename folder and add public

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import Nav from "@/components/Nav";
 
-import "@/public/styles/font.css";
-import "@/public/styles/global.css";
+import "@/assets/styles/font.css";
+import "@/assets/styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/utils/arts.ts
+++ src/utils/arts.ts
@@ -1,6 +1,6 @@
-import file01 from "@/public/art/mypic.jpg";
-import file02 from "@/public/art/mypic2.jpg";
-import file03 from "@/public/art/mypic3.jpg";
+import file01 from "@/assets/art/mypic.jpg";
+import file02 from "@/assets/art/mypic2.jpg";
+import file03 from "@/assets/art/mypic3.jpg";
 
 export const images = [file01, file02, file03];
 
